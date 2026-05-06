816d36354134a12d34d0c24af3a9b670fe72f8d4
VarmaOfficialRepo <varmajiangad@gmail.com>
2026-05-06 21:29:04 +0530

    Setup CI/CD Pipeline with JUnit and Docker

---
+++ src/main/java/com/devops/App.java
@@ -0,0 +1,11 @@
+package com.devops;
+
+public class App {
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
